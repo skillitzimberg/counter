d4e8794024ba11806a8feb1987a1d862ec0a2b28
Robert Lee and Scott Bergler <scott@>
2018-10-23 11:09:35 -0700

    Add user input error checking

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,32 @@ $(document).ready(function(){
 
     var outputArray = [];
 
+    console.log(countToNum);
+    console.log(countByNum);
 
-for (var index = countByNum; index <= countToNum; index += countByNum) {
-  outputArray.push(index);
-  console.log(outputArray);
-  //debugger;
-  // console.log("index=" + index);
-  // console.log("countToNum=" + countToNum);
-  // console.log("countByNum=" + countByNum);
+    if (isNaN(countToNum) || isNaN(countByNum)) {
+      debugger;
+      $("span#outputValueHere").text("You're awful. Try again.");
+    } else if (countToNum < 0 || countByNum < 0 ) {
+      $("span#outputValueHere").text("No negatives.");
+    } else if (countByNum > countToNum) {
+      $("span#outputValueHere").text("Reverse those numbers!");
+    } else {
+        for (var index = countByNum; index <= countToNum; index += countByNum) {
+          outputArray.push(index);
+          console.log(outputArray);
+          //debugger;
+          // console.log("index=" + index);
+          // console.log("countToNum=" + countToNum);
+          // console.log("countByNum=" + countByNum);
 
-  }
+          }
 
-  $("span#outputValueHere").text(outputArray);
+          $("span#outputValueHere").text(outputArray);
 
+        }
 
-  event.preventDefault();
+    event.preventDefault();
   }); /*This is the end of the button submit event.*/
 
 
